78c36803e0837b2594ffacd3983619b539120e23
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2022-03-25 17:43:38 +0600

    added all

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Azure, RSalehin24!')
+});
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Server is listening to the port 8080 ...')
+})
